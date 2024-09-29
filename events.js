279328e9a048cdf4cc0ceff242a6a3eea6481b1f
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-09-29 20:02:37 +0300

    Add event listener, basic concept ui, styles

--- events.js
+++ events.js
@@ -2,24 +2,32 @@ import { Player } from "./player.js";
 
 let p1, p2;
 
-document.getElementById("start").addEventListener("click", startGame);
+document.getElementById("start").addEventListener("click", () => {
+  document.querySelector("form").classList.remove("hidden");
+});
 
-function startGame() {
-  let answer = isTwoPlayerGame();
-  createPlayers(answer);
+document.querySelector("form").onsubmit = (e) => {
+  e.preventDefault();
+  const p1Name = document.getElementById("p1Name");
+  const p2Name = document.getElementById("p2Name");
+  p1 = new Player(p1Name.textContent);
+  p2 = new Player(p2Name.textContent);
+  p1Name.value = "";
+  p2Name.value = "";
+  document.querySelector("form").classList.add("hidden");
   //render boards
-}
-
-function isTwoPlayerGame() {
-  let answer = "";
-  do {
-    answer = prompt("Will you play vs another player [YES]/[NO]").toUpperCase();
-  } while (!["YES", "NO"].includes(answer));
-  return answer === "YES" ? true : false;
-}
-function createPlayers(answer) {
-  let p1Name = prompt("What is Player 1's name?");
-  let p2Name = answer ? prompt("What is Player 1's name?") : "";
-  p1 = new Player(p1Name);
-  p2 = new Player(p2Name);
-}
+};
+document.querySelector(".enemyBoard").addEventListener("click", (e) => {
+  const row = e.target.dataset.row;
+  const column = e.target.dataset.column;
+  document.getElementById(
+    "playing"
+  ).textContent = `You clicked on row ${row} and column ${column}`;
+  const clickedCell = document.querySelector(
+    `[data-row="${row}"][data-column="${column}"]`
+  );
+  // clicked cell text content should be in draw module
+  clickedCell.textContent = "X";
+  clickedCell.classList.add("clicked");
+  //render boards (where missed textcontent = “💦” (U+1F4A6) , else if success = “🔥” (U+1F525) or w/e)
+});
